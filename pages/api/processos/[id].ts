a38f7a30df4e5f07839dd5038dab205e23b81093
Thyago Carvalho <othyagocarvalho@outlook.com>
2022-08-02 09:45:57 -0300

    remove unused imports

--- pages/api/processos/[id].ts
+++ pages/api/processos/[id].ts
@@ -1,10 +1,6 @@
 import { NextApiResponse, NextApiRequest } from 'next';
-import { processos } from '../../../data';
-import { Processo } from '../../../interfaces';
 import { fauna } from '../../../services/fauna';
 import { query as q } from 'faunadb';
-
-// let processo1 = processos[1];
 
 export default async function handler(
     req: NextApiRequest,
@@ -14,7 +10,6 @@ export default async function handler(
     const { query } = req;
     const { id } = query;
 
-
     console.log(query);
 
     await fauna
@@ -22,5 +17,7 @@ export default async function handler(
         .then(ret => (lawsuitData = ret))
         .then(err => console.log(err));
 
-    return lawsuitData ? res.status(200).json(lawsuitData) : res.status(404).json({ error: 'Processo não encontrado'});
+    return lawsuitData
+        ? res.status(200).json(lawsuitData)
+        : res.status(404).json({ error: 'Processo não encontrado' });
 }
